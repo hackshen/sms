8073848339260a10645dfa1090750815be42b250
hackshen <hackshen.com@gmail.com>
2018-01-26 11:46:38 +0800

    sms

---
+++ app.js
@@ -0,0 +1,33 @@
+/**
+ * 云通信基础能力业务短信发送、查询详情以及消费消息示例，供参考。
+ * Created on 2017-07-31
+ */
+
+const SMSClient = require('@alicloud/sms-sdk')
+
+// ACCESS_KEY_ID/ACCESS_KEY_SECRET 根据实际申请的账号信息进行替换
+const accessKeyId = 'xxxx'
+const secretAccessKey = 'xxxxx'
+
+//在云通信页面开通相应业务消息后，就能在页面上获得对应的queueName,不用填最后面一段
+const queueName = 'Alicom-Queue-1092397003988387-'
+
+//初始化sms_client
+let smsClient = new SMSClient({accessKeyId, secretAccessKey})
+
+//发送短信
+
+smsClient.sendSMS({
+    PhoneNumbers: 'xxxxx',    //手机号
+    SignName: 'xxxx',     //短信签名
+    TemplateCode: 'xxxx',  //短信模板
+    TemplateParam: '{"number":"999999"}'  //模板参数
+}).then(function (res) {
+    let {Code}=res
+    if (Code === 'OK') {
+        //处理返回参数
+        console.log(res)
+    }
+}, function (err) {
+    console.log(err)
+})
